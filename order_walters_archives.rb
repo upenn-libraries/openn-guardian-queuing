ea54f1c42a891255dc12f7878e0fca2fa7d546b7
Doug Emery <emeryr@upenn.edu>
2019-04-12 13:51:41 -0400

    Initial commit

---
+++ order_walters_archives.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+require 'csv'
+
+input_csv = File.expand_path '../data/Glacier_sizes.csv', __FILE__
+
+data_blocks = {}
+GROUPS = %i{ a b c d }
+
+CSV.foreach(input_csv, headers: true).with_index do |row,index|
+  # binding.pry
+  break if row['size_in_gb'].to_f > 100
+  if index < 40
+    (data_blocks[:first_set] ||= []) << row.to_hash
+  else
+    g_index = ((index - 39)/100) % GROUPS.size
+    group = GROUPS[g_index]
+    (data_blocks[group] ||= []) << row.to_hash
+  end
+end
+headers = %w{ size size_in_gb directory group }
+
+position = 0
+CSV headers: true do |csv|
+  csv << headers
+  data_blocks[:first_set].each do |row|
+    row['group']  = :first_set
+    csv << row
+  end
+  (0..).each do |i|
+    rows = GROUPS.map { |group|
+      hash = data_blocks[group][i]
+      if hash.nil?
+        nil
+      else
+        hash.merge('group' => group)
+      end
+    }.compact
+    break if rows.empty?
+    rows.each { |row| csv << row }
+  end
+end
+
