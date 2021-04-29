1eda4e10f81b6b87e750cf43f8b7c23bf9976e70
Doug Emery <emeryr@upenn.edu>
2021-04-29 12:01:16 -0400

    WIP: start script

---
+++ scripts/find_new_on_glacier.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+##
+#
+# This script is used to generate the list of documents on the 
+# OPenn scratch drive that are in glacier and can be deleted
+# from scratch storage.
+#
+# From the Glacier Priorities list 
